7dc332f2f5f7fa554377684066acd8e26f514014
tominator633 <t-ruz@seznam.cz>
2024-04-07 12:43:23 +0200

    file changes

--- JS/nav.js
+++ JS/nav.js
@@ -3,16 +3,16 @@
 /* ###############  Nav buttons ############# */
 let navButton = document.getElementById("navigation-button");
 let navCloseButton = document.getElementById("navigation-close-button");
-let header = document.getElementsByTagName("header")[0];
+let mobileNav = document.getElementById("mobileNav");
 /* Přidej ty vars do funkce (kvuli polution) */
 function openHeader () {
     navButton.style.display = "none";
-    header.style.display = "flex";
+    mobileNav.style.display = "flex";
 }
 navButton.addEventListener("click",openHeader);
 
 function closeHeader () {
-    header.style.display = "none";
+    mobileNav.style.display = "none";
     navButton.style.display = "block"
 }
 navCloseButton.addEventListener("click",closeHeader);
