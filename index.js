bf8d9e75a46bbe93a7cf1b0a71924a11467bebbd
tominator633 <t-ruz@seznam.cz>
2024-04-07 09:58:27 +0200

    new layout base for about section

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let languageSelected = document.querySelector("#language-options a:nth-child(3)"
 
 function showLanguageOptions () {
     languageOptions.style.clipPath = "polygon(10% 0, 90% 0, 100% 20%, 100% 80%, 90% 100%, 10% 100%, 0 80%, 0 20%)";
-    languageOptions.style.backgroundColor = "#001E6C";
+    languageOptions.style.backgroundColor = "#333";
     languageOne.style.display = "flex";
     languageTwo.style.display = "flex";
 }
